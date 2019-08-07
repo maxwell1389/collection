0c12e1be856739831894ba7b07b1662daf815573
Maxwell <maxwell1389@sina.com>
2019-08-07 18:00:11 +0800

    上传工程

---
+++ list/src/main/java/com/github/maxwell/collection/NoSuchElementException.java
@@ -0,0 +1,12 @@
+package com.github.maxwell.collection;
+
+public class NoSuchElementException extends RuntimeException {
+
+    public NoSuchElementException() {
+        super();
+    }
+
+    public NoSuchElementException(String s) {
+        super(s);
+    }
+}
